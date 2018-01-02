3c1841f6cea44cfe973a278c662d1ad3c1e4f7cd
msanchezperez1 <35042556+msanchezperez1@users.noreply.github.com>
2018-01-02 12:43:35 -0800

    Add files via upload

---
+++ TestIt.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public TestIt{
+
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in());
+		int n = 5;
+		System.out.println("The number equals " + n);
+	}
+
+}
